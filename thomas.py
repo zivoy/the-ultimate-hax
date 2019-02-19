4a4c4920b3ef50422112a19713f36de704c700c4
root <ziv132@gmail.com>
2019-02-19 17:06:30 +0000

    new file:   thomas.py

---
+++ thomas.py
@@ -0,0 +1,15 @@
+class classofclans:
+	password= "yes"
+	def __init__(self):
+		print("passwordedd")
+
+	def hack(self):
+		for tnite in range(100000):
+			print(str(tnite)+"% hacked")
+		print(self.password)
+
+password= classofclans()
+
+icsgrade = 10000000
+
+password.hack()
